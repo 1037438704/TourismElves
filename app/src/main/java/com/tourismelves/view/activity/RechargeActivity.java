e0e76a6d244eb628368c29b9ac1a27086d4774eb
fanhui <fhandr@163.com>
2018-08-21 16:03:25 +0800

    更新顶部11

--- app/src/main/java/com/tourismelves/view/activity/RechargeActivity.java
+++ app/src/main/java/com/tourismelves/view/activity/RechargeActivity.java
@@ -101,7 +101,6 @@ public class RechargeActivity extends StateBaseActivity {
                                 } catch (JSONException e) {
                                     e.printStackTrace();
                                 }
-
                             }
                         });
             }
