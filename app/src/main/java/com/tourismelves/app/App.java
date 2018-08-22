7fd20da021e428cd6a0457e275b754b5b1b60167
111 <2207601851@qq.com>
2018-08-22 12:23:06 +0800

    黄波

--- app/src/main/java/com/tourismelves/app/App.java
+++ app/src/main/java/com/tourismelves/app/App.java
@@ -12,8 +12,7 @@ import com.tourismelves.utils.system.LocationUtil;
 //MD5: 2f31408314c3fd9edaef89d9b0709c2f
 //SHA1: 46:DF:DF:AA:3E:6F:40:31:D4:81:AD:B8:9B:6C:86:6C:4A:CA:BB:C7
 //SHA256: BD:EE:A1:6B:E5:C3:CF:48:1D:DF:29:88:0F:7F:E2:D1:6E:AC:F8:DD:FD:BF:4F:C4:64:2A:FC:F2:8D:44:20:F4
-
-
+//
 public class App extends Application {
     private static Context mContext;
 
